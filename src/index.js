e63e59415051dac64f98bb87daa1bea037175e65
lxpvlv <w.lxpvlv@gmail.com>
2020-03-03 02:23:29 +0500

    feat: add solution for typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    return Array.isArray(array)
+        ? array.reduce((min, item) => (item < min ? item : min), 0)
+        : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return Array.isArray(array)
+        ? array.reduce((max, item) => (item > max ? item : max), 0)
+        : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return Array.isArray(array) && array.length !== 0
+        ? array.reduce((sum, item) => sum + item, 0) / array.length
+        : 0;
+};
